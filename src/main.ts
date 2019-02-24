62c6d6d046f30e6a024e36a805f8a088ecaaacbf
Sunny Edogbo <sunny.edogbo@andela.com>
2019-02-24 20:56:00 +0100

    [feature]post crud implementation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+const NEST_PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(3000);
+  await app.listen(NEST_PORT);
 }
 bootstrap();
